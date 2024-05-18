337e812c5c8624bfa1e5e1e8d765f7d10319a63c
Katie <katiemlim@gmail.com>
2024-05-18 01:45:30 -0700

    Update dependencies.sh

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -28,6 +28,9 @@ pip3 install fusesoc
 
 # install Verilator dependencies
 sudo apt-get install -y help2man perl python3 make autoconf g++ flex bison ccache
+sudo apt-get install -y libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install -y libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install -y zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
 
 
 

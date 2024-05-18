331b4c2d441b16779cccf3e3c04f5a30b87c069f
Katie <katiemlim@gmail.com>
2024-05-18 01:55:55 -0700

    Update dependencies.sh

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -30,7 +30,6 @@ pip3 install fusesoc
 sudo apt-get install -y help2man perl python3 make autoconf g++ flex bison ccache
 sudo apt-get install -y libfl2  # Ubuntu only (ignore if gives error)
 sudo apt-get install -y libfl-dev  # Ubuntu only (ignore if gives error)
-sudo apt-get install -y zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
 
 
 

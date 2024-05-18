428956245b818041a63a5f89050fbe9793c4e5dd
Katie <katiemlim@gmail.com>
2024-05-18 11:02:43 -0700

    Update dependencies.sh
    Move Verilator dependencies to their own file

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -26,10 +26,6 @@ pip3 install cocotb_bus
 pip3 install scapy
 pip3 install fusesoc
 
-# install Verilator dependencies
-sudo apt-get install -y help2man perl python3 make autoconf g++ flex bison ccache
-sudo apt-get install -y libfl2  # Ubuntu only (ignore if gives error)
-sudo apt-get install -y libfl-dev  # Ubuntu only (ignore if gives error)
 
 
 

c4e063cb883d57cc653d7fe5b7df6790b68697f4
Gen Tamada <gtamada@ucsb.edu>
2026-06-10 03:19:28 -0700

    util: add INIT_TO_ONE param to valid_bitvector

--- util/common/valid_bitvector.sv
+++ util/common/valid_bitvector.sv
@@ -1,6 +1,9 @@
 module valid_bitvector #(
      parameter BITVECTOR_SIZE = 64
     ,parameter BITVECTOR_INDEX_W = $clog2(BITVECTOR_SIZE)
+    // When 1, the vector resets to all-ones. Default 0 preserves the original
+    // reset-to-zero behavior for existing instantiations that don't set it.
+    ,parameter INIT_TO_ONE = 0
 )(
      input clk
     ,input rst
@@ -24,7 +27,12 @@ module valid_bitvector #(
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            valid_bits_reg <= '0;
+            if (INIT_TO_ONE) begin
+                valid_bits_reg <= '1;
+            end
+            else begin
+                valid_bits_reg <= '0;
+            end
         end
         else begin
             valid_bits_reg <= valid_bits_next;
